4925878033aef4fd7a6b173b95548c827e741e47
Brun41v35 <alves.bruno945@gmail.com>
2023-07-22 16:07:06 -0300

    Delivers invalidData error on 200 HTTPResponse

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -61,6 +61,17 @@ class RemoteFeedLoaderTests: XCTestCase {
                             at: index)
             XCTAssertEqual(capturedErrors, [.invalidData])
         }
+        
+        func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load { capturedErrors.append($0) }
+            
+            let invalidJSON = Data("invalid json".utf8)
+            
+            client.complete(withStatusCode: 200,
+                            data: invalidJSON)
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
     }
     
     // MARK: - Helper
@@ -91,12 +102,13 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
         
         func complete(withStatusCode code: Int,
+                      data: Data = Data(),
                       at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index],
                                            statusCode: code,
                                            httpVersion: nil,
                                            headerFields: nil)!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
     }
 }

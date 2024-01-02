3a5f602bb6ad0fbb79720d152e84c35a5e75181f
Bruno Alves <alves.bruno945@gmail.com>
2024-01-02 20:23:28 -0300

    Move `FeedStore` to own file

---
+++ EssentialFeed/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Bruno Silva on 2024-01-02.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timeStamp: Date, completion: @escaping InsertionCompletion)
+}

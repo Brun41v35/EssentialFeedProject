f377b64a81b66ce2ce01bcc09481ff8d0f7a84d1
Bruno Alves <alves.bruno945@gmail.com>
2024-01-24 18:44:41 -0300

    Retrieving from invalid cache twice

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -96,7 +96,7 @@ class CodableFeedStoreTests: XCTestCase {
 
     func test_retrieve_deliversFailureOnRetrievalError() {
         let storeURL = testSpecificStoreURL()
-        let sut = makeSUT()
+        let sut = makeSUT(storeURL: storeURL)
 
         try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
 
@@ -121,6 +121,15 @@ class CodableFeedStoreTests: XCTestCase {
         insert((feed, timestamp), to: sut)
 
         expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
+    }
+
+    func test_retrieve_hasNoSideEffectsOnFailure() {
+        let storeURL = testSpecificStoreURL()
+        let sut = makeSUT(storeURL: storeURL)
+
+        try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
+
+        expect(sut, toRetrieve: .failure(anyNSError()))
     }
 
     // MARK: - Helpers

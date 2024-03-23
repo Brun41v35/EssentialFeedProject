0ab4df8e9e9172c2ddd52da4a8fd0284e6d96d6e
Bruno Alves <alves.bruno945@gmail.com>
2024-03-23 13:01:49 -0300

    Add `EssentialFeedCacheIntegrationTests` target to separate

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,10 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Bruno Silva on 23/03/24.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {}

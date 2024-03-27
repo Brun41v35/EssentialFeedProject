552484c1590a0eb8a41da2b913f59864899b898f
Bruno Alves <alves.bruno945@gmail.com>
2024-03-26 22:40:10 -0300

    Show loading indicator on view did load

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -4,7 +4,6 @@ import UIKit
 import EssentialFeed
 
 final class FeedViewController: UITableViewController {
-
     private var loader: FeedLoader?
 
     convenience init(loader: FeedLoader) {
@@ -14,9 +13,10 @@ final class FeedViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        refreshControl?.beginRefreshing()
         load()
     }
 
@@ -37,19 +37,32 @@ final class FeedViewControllerTests: XCTestCase {
         let (sut, loader) = makeSUT()
 
         sut.loadViewIfNeeded()
-        
+
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+
+    func test_pullToRefresh_loadsFeed() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+    
         sut.refreshControl?.simulatePullToRefresh()
         XCTAssertEqual(loader.loadCallCount, 2)
-        
+    
         sut.refreshControl?.simulatePullToRefresh()
         XCTAssertEqual(loader.loadCallCount, 3)
     }
 
+    func test_viewDidLoad_showsLoadingIndicator() {
+        let (sut, _) = makeSUT()
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
+    }
+
     // MARK: - Helpers
 
-    private func makeSUT(file: StaticString = #file,
-                         line: UInt = #line) -> (sut: FeedViewController,
-                                                 loader: LoaderSpy) {
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
         let loader = LoaderSpy()
         let sut = FeedViewController(loader: loader)
         trackForMemoryLeak(loader, file: file, line: line)
@@ -58,15 +71,13 @@ final class FeedViewControllerTests: XCTestCase {
     }
 
     final class LoaderSpy: FeedLoader {
-        private(set) var loadCallCount = 0
+        private(set) var loadCallCount: Int = 0
 
         func load(completion: @escaping (FeedLoader.Result) -> Void) {
             loadCallCount += 1
         }
     }
 }
-
-// MARK: - UIRefreshControl
 
 private extension UIRefreshControl {
     func simulatePullToRefresh() {

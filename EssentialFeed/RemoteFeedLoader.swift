0c5cc1c610236e34b74f57581c263824936386aa
Bruno Alves <alves.bruno945@gmail.com>
2024-06-13 09:25:43 -0300

    Feed image view loading indicator is visible while loading image

--- EssentialFeed/RemoteFeedLoader.swift
+++ EssentialFeed/RemoteFeedLoader.swift
@@ -22,7 +22,7 @@ public final class RemoteFeedLoader: FeedLoader {
         self.client = client
         self.url = url
     }
-    
+
     public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { [weak self] result in
             guard self != nil else { return }

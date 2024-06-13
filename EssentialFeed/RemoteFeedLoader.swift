2466aee2aa85a39b97846b2fe48738bbd681e0b5
Bruno Alves <alves.bruno945@gmail.com>
2024-06-13 08:46:35 -0300

    Add space blank at `RemoteFeedLoader`

--- EssentialFeed/RemoteFeedLoader.swift
+++ EssentialFeed/RemoteFeedLoader.swift
@@ -10,14 +10,14 @@ import Foundation
 public final class RemoteFeedLoader: FeedLoader {
     private let url: URL
     private let client: HTTPClient
-    
+
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
     }
-    
+
     public typealias Result = FeedLoader.Result
-    
+
     public init(url: URL, client: HTTPClient) {
         self.client = client
         self.url = url
@@ -26,7 +26,7 @@ public final class RemoteFeedLoader: FeedLoader {
     public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { [weak self] result in
             guard self != nil else { return }
-            
+
             switch result {
             case let .success((data, response)):
                 completion(RemoteFeedLoader.map(data, from: response))
@@ -49,8 +49,8 @@ public final class RemoteFeedLoader: FeedLoader {
 private extension Array where Element == RemoteFeedItem {
     func toModels() -> [FeedImage] {
         return map { FeedImage(id: $0.id,
-                              description: $0.description,
-                              location: $0.location,
-                              url: $0.image) }
+                               description: $0.description,
+                               location: $0.location,
+                               url: $0.image) }
     }
 }

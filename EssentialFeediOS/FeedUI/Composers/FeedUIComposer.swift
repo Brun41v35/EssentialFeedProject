042d53ef9deed547e703d1d8e5fe896e27bdac72
Bruno Alves <alves.bruno945@gmail.com>
2024-06-15 13:23:34 -0300

    Decouple FeedImageViewModel from UIKit by creating a transformer

--- EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -12,7 +12,8 @@ public final class FeedUIComposer {
 
     private init() {}
 
-    public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
+    public static func feedComposedWith(feedLoader: FeedLoader,
+                                        imageLoader: FeedImageDataLoader) -> FeedViewController {
         let feedViewModel = FeedViewModel(feedLoader: feedLoader)
         let refreshController = FeedRefreshViewController(viewModel: feedViewModel)
         let feedController = FeedViewController(refreshController: refreshController)
@@ -20,10 +21,13 @@ public final class FeedUIComposer {
         return feedController
     }
 
-    private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
+    private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController,
+                                                   loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
         return { [weak controller] feed in
             controller?.tableModel = feed.map { model in
-                FeedImageCellController(model: model, imageLoader: loader)
+                FeedImageCellController(viewModel: FeedImageViewModel(model: model,
+                                                                      imageLoader: loader,
+                                                                      imageTransformer: UIImage.init))
             }
         }
     }

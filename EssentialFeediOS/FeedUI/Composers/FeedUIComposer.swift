d5c54f1fb84c3e24deaaf38feb9b71183860dd86
Bruno Alves <alves.bruno945@gmail.com>
2024-06-17 17:29:16 -0300

    Move from property injection to constructor injection in the `FeedPresenter`

--- EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -9,20 +9,20 @@ import UIKit
 import EssentialFeed
 
 public final class FeedUIComposer {
-
+    
     private init() {}
-
+    
     public static func feedComposedWith(feedLoader: FeedLoader,
                                         imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let presenter = FeedPresenter()
-        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader, presenter: presenter)
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
         let refreshController = FeedRefreshViewController(delegate: presentationAdapter)
         let feedController = FeedViewController(refreshController: refreshController)
-        presenter.loadingView = WeakRefVirtualProxy(refreshController)
-        presenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
+        presentationAdapter.presenter = FeedPresenter(feedView: FeedViewAdapter(controller: feedController,
+                                                                                imageLoader: imageLoader),
+                                                      loadingView: WeakRefVirtualProxy(refreshController))
         return feedController
     }
-
+    
     private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController,
                                                    loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
         return { [weak controller] feed in
@@ -38,7 +38,7 @@ public final class FeedUIComposer {
 
 private final class WeakRefVirtualProxy<T: AnyObject> {
     private weak var object: T?
-
+    
     init(_ object: T) {
         self.object = object
     }
@@ -53,12 +53,12 @@ extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
 private final class FeedViewAdapter: FeedView {
     private weak var controller: FeedViewController?
     private let imageLoader: FeedImageDataLoader
-
+    
     init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
         self.controller = controller
         self.imageLoader = imageLoader
     }
-
+    
     func display(_ viewModel: FeedViewModel) {
         controller?.tableModel = viewModel.feed.map { model in
             FeedImageCellController(viewModel:
@@ -70,25 +70,24 @@ private final class FeedViewAdapter: FeedView {
 }
 
 private final class FeedLoaderPresentationAdapter: FeedRefreshViewControllerDelegate {
-
+    
     private let feedLoader: FeedLoader
-    private let presenter: FeedPresenter
-
-    init(feedLoader: FeedLoader, presenter: FeedPresenter) {
+    var presenter: FeedPresenter?
+    
+    init(feedLoader: FeedLoader) {
         self.feedLoader = feedLoader
-        self.presenter = presenter
     }
-
+    
     func didRequestFeedRefresh() {
-        presenter.didStartLoadingFeed()
-
+        presenter?.didStartLoadingFeed()
+        
         feedLoader.load { [weak self] result in
             switch result {
             case let .success(feed):
-                self?.presenter.didFinishLoadingFeed(with: feed)
-
+                self?.presenter?.didFinishLoadingFeed(with: feed)
+                
             case let .failure(error):
-                self?.presenter.didFinishLoadingFeed(with: error)
+                self?.presenter?.didFinishLoadingFeed(with: error)
             }
         }
     }

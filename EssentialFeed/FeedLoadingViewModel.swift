957c4deb7b30fb40750e0b6dcab8f3091257f395
Bruno Alves <alves.bruno945@gmail.com>
2024-07-02 20:56:53 -0300

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeed/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Bruno Silva on 02/07/24.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
